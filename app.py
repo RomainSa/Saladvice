ebb10c870bd45b3b8cad7e12178e27e0a265c101
romain.savidan <romain.savidan@cdiscount.com>
2018-02-16 23:21:33 +0100

    POC on one option

--- app.py
+++ app.py
@@ -3,12 +3,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 
-@app.route('/', methods=["GET","POST"])
-def hello_world():
-    if request.method == 'POST':
-        return render_template('index.html', colours=[('red', False), ('green', False), ('blue', False), ('black', False)], notes=['do', 're', 'mi'], selected=True)
-    else:
-        return render_template('index.html', colours=[('red', False), ('green', False), ('blue', False)], notes=['do', 're', 'mi'])
+@app.route('/', methods=["GET", "POST"])
+def index():
+    n_ingredients = request.form['n_ingredients'] if 'n_ingredients' in request.form else '4'
+    save_n_ingredients = 'save_n_ingredients' in request.form
+    return render_template('index.html',
+                           n_ingredients_list=['4', '5', '6'],   # must be str since html will send option value (=str)
+                           n_ingredients=n_ingredients,
+                           save_n_ingredients=save_n_ingredients)
 
 
 if __name__ == '__main__':
